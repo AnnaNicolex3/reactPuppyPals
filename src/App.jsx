da3883c96594a82aeae41b2c640d78990bf35ede
LeAnna <leanna.nicole@yahoo.com>
2024-03-07 19:08:07 -0800

    Rendered Puppies

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,23 @@ import{ useState } from 'react'
 function App() {
 const [puppies, setPuppies] =useState(puppyList)
 const [featPupId, setFeatPupId] = useState(null)
+const featPup = puppies.find(puppy=>puppy.id === featuredPupId)
   console.log(puppyList, featPupId)
 
   return (
     <>
    {puppies.map((puppy,i)=> <p key ={puppy.id} onClick={()=>setFeatPupId(puppy.id)}>{puppy.name}</p>)}
-   
+   {featuredPupId ?
+    <div>
+    <h2>{featuredPup.name}</h2>
+    <ul>
+      <li>Age: {featuredPup.age}</li>
+      <li>Email: {featuredPup.email}</li>
+    </ul>
+  </div>
+   :
+   <p>Please select a puppy!</p>
+   }
     </>
   )
 }
